4d0ab2d931f368968d1c95f14de960263ae01ca2
Blade <649349299@qq.com>
2017-07-12 15:08:39 +0800

    调整样式 thymeleaf demo

--- annie-shop-platform/src/main/java/com/annie/controller/ProductTypeController.java
+++ annie-shop-platform/src/main/java/com/annie/controller/ProductTypeController.java
@@ -1,11 +1,12 @@
 package com.annie.controller;
 
 import com.annie.constant.Constant;
-import com.annie.dto.ResultDto;
 import com.annie.entity.ProductType;
 import com.annie.service.ProductTypeService;
 import com.github.pagehelper.PageInfo;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
@@ -24,21 +25,20 @@ public class ProductTypeController {
     private final String VIEW_PATH = "/html/productType/";
 
     @RequestMapping(value = "/findProductTypePage")
-    public ResultDto<PageInfo<ProductType>> findProductTypePage(@RequestParam(value = "pageSize", defaultValue = Constant.PAGE_SIZE) int pageSize,
-                                                                 @RequestParam(value = "pageNum", defaultValue = Constant.PAGE_NUM) int pageNum){
-        ResultDto<PageInfo<ProductType>> resultDto=new ResultDto<PageInfo<ProductType>>();
+    public ModelAndView findProductTypePage(@RequestParam(value = "pageSize", defaultValue = Constant.PAGE_SIZE) Integer pageSize,
+                                            @RequestParam(value = "pageNum", defaultValue = Constant.PAGE_NUM) Integer pageNum) {
         PageInfo<ProductType> productTypePageInfo = productTypeService.findProductTypePage(null, pageSize, pageNum);
-        resultDto.setResultData(productTypePageInfo);
-        resultDto.setResultCode("");
-        resultDto.setResultMsg("");
         ModelAndView mv = new ModelAndView();
-        mv.addObject(productTypePageInfo);
+        mv.addObject("productTypePageInfo", productTypePageInfo);
         mv.setViewName(VIEW_PATH + "product_type_list");
-        return resultDto;
+        return mv;
     }
 
     @RequestMapping(value = "/create")
-    public void createProductType(@RequestParam("typeName") String typeName, @RequestParam("parentId") Integer parentId, @RequestParam("isUse") String isUse, @RequestParam("picPath") String picPath) {
+    public void createProductType(@RequestParam("typeName") String typeName,
+                                  @RequestParam("parentId") Integer parentId,
+                                  @RequestParam("isUse") String isUse,
+                                  @RequestParam("picPath") String picPath) {
         ProductType productType = new ProductType();
         productType.setTypeName(typeName);
         productType.setParentId(parentId);
