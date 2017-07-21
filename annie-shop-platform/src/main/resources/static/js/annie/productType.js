1e36ace1c3d371d2d7c96935d7ad25568ba5f467
Blade <649349299@qq.com>
2017-07-21 18:05:46 +0800

    删除功能js封装

--- annie-shop-platform/src/main/resources/static/js/annie/productType.js
+++ annie-shop-platform/src/main/resources/static/js/annie/productType.js
@@ -35,33 +35,11 @@ function updateProductType() {
 }
 
 function deleteProductType(productTypeId) {
-    swal({
-        title: "您确定要删除这条信息吗",
-        text: "删除后将无法恢复，请谨慎操作！",
-        type: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#DD6B55",
-        confirmButtonText: "删除",
-        closeOnConfirm: false
-    }, function () {
-        var url = "/productType/deleteProductType";
-        var params = {
-            productTypeId: productTypeId
-        };
-        $.ajax({
-            type : "POST",
-            url : url,
-            async : true,
-            global : true,
-            data : params, // 要传递的数据
-            success : function(data) {
-                var resultCode = data.resultCode;
-                if(resultCode == '0000'){
-                    swal("删除成功！", "您已经永久删除了这条信息。", "success");
-                }else {
-                    swal("删除失败！", "", "error");
-                }
-            }
-        });
-    });
+
+    var url = "/productType/deleteProductType";
+    var params = {
+        productTypeId: productTypeId
+    };
+
+    confirm(url, params, closeModal);
 }
--- annie-shop-platform/src/main/resources/static/js/public.js
+++ annie-shop-platform/src/main/resources/static/js/public.js
@@ -25,6 +25,35 @@ function formSubmit(formId, url, complete) {
     $("#" + formId).ajaxSubmit(options);
 }
 
+function confirm(url, params, complete) {
+    swal({
+        title: "您确定要删除这条信息吗",
+        text: "删除后将无法恢复，请谨慎操作！",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "删除",
+        closeOnConfirm: false
+    }, function () {
+        $.ajax({
+            type : "POST",
+            url : url,
+            async : true,
+            global : true,
+            data : params, // 要传递的数据
+            success : function(data) {
+                var resultCode = data.resultCode;
+                if(resultCode == '0000'){
+                    // swal("删除成功！", "您已经永久删除了这条信息。", "success");
+                    complete();
+                }else {
+                    swal("删除失败！", "", "error");
+                }
+            }
+        });
+    });
+}
+
 function successAlert(msg, complete) {
     // window.location.reload(true);
     swal("操作成功！", msg, "success", complete);
